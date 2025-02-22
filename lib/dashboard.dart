8ecfffb675cdfd37376705dc003e10057e00d1ca
unknown <amaloo6282@gmail.com>
2025-02-22 15:20:13 +0530

    dashboard screen added

---
+++ lib/dashboard.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 35,
+            ),
+            Text(
+              'Dasboard',
+              style: TextStyle(fontSize: 35, color: Color(0xFF7F00FF)),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 30),
+              child: Row(
+                children: [
+                  DashboardCard(
+                      label: '150',
+                      label1: 'Total Bookings',
+                      iconData: Icons.library_books),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  DashboardCard(
+                      label: '53',
+                      label1: 'Total Parking Slot',
+                      iconData: Icons.local_parking_sharp),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  DashboardCard(
+                      label: '50',
+                      label1: 'Total Vehicle',
+                      iconData: Icons.car_repair),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  DashboardCard(
+                      label: '50',
+                      label1: 'Vehicle Owner',
+                      iconData: Icons.supervisor_account_outlined)
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class DashboardCard extends StatelessWidget {
+  final String label;
+  final String label1;
+  final IconData iconData;
+  const DashboardCard({
+    super.key,
+    required this.label,
+    required this.label1,
+    required this.iconData,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      foregroundDecoration: BoxDecoration(
+          border: Border.all(color: Color(0xFF00A36C)),
+          borderRadius: BorderRadius.all(Radius.circular(15))),
+      decoration: BoxDecoration(
+          color: Color(0xFF7F00FF),
+          borderRadius: BorderRadius.all(Radius.circular(15))),
+      height: 150,
+      width: 250,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 0),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      label,
+                      style: TextStyle(fontSize: 40, color: Colors.white),
+                    ),
+                    Text(
+                      label1,
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                  ],
+                ),
+                Spacer(),
+                Icon(
+                  iconData,
+                  size: 50,
+                  color: Colors.white,
+                )
+              ],
+            ),
+            Spacer(),
+            TextButton(
+                onPressed: () {},
+                child: Text(
+                  'More Info ->',
+                  style: TextStyle(fontSize: 18, color: Colors.white),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/homescreen.dart
@@ -0,0 +1,185 @@
+import 'package:flutter/material.dart';
+import 'package:spoton_admin/dashboard.dart';
+import 'package:spoton_admin/loginpage.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 6, vsync: this, initialIndex: 0);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: 250,
+            color: Colors.black,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 60.0,
+                ),
+                Text(
+                  'SpotOn',
+                  style: TextStyle(fontSize: 25, color: Color(0xFF7F00FF)),
+                ),
+                SizedBox(height: 80.0),
+                DrawerItemButton(
+                  inverse: _tabController.index == 0,
+                  iconData: Icons.dashboard,
+                  label: 'Dashboard',
+                  onTap: () {
+                    _tabController.animateTo(0);
+                    setState(() {});
+                  },
+                ),
+                DrawerItemButton(
+                  inverse: _tabController.index == 1,
+                  iconData: Icons.car_repair,
+                  label: 'Booking',
+                  onTap: () {
+                    _tabController.animateTo(1);
+                    setState(() {});
+                  },
+                ),
+                DrawerItemButton(
+                  inverse: _tabController.index == 2,
+                  iconData: Icons.local_parking_sharp,
+                  label: 'Parking Spaces',
+                  onTap: () {
+                    _tabController.animateTo(2);
+                    setState(() {});
+                  },
+                ),
+                DrawerItemButton(
+                  inverse: _tabController.index == 3,
+                  iconData: Icons.report,
+                  label: 'Reports',
+                  onTap: () {
+                    _tabController.animateTo(3);
+                    setState(() {});
+                  },
+                ),
+                DrawerItemButton(
+                  inverse: _tabController.index == 4,
+                  iconData: Icons.report_problem_outlined,
+                  label: 'Problems',
+                  onTap: () {
+                    _tabController.animateTo(4);
+                    setState(() {});
+                  },
+                ),
+                DrawerItemButton(
+                  inverse: _tabController.index == 5,
+                  iconData: Icons.pending_actions_sharp,
+                  label: 'Roles & Permissions',
+                  onTap: () {
+                    _tabController.animateTo(5);
+                    setState(() {});
+                  },
+                ),
+                Spacer(),
+                DrawerItemButton(
+                  inverse: false,
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginScreen()),
+                    );
+                  },
+                  iconData: Icons.logout,
+                  label: "Log out",
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: TabBarView(
+              physics: NeverScrollableScrollPhysics(),
+              controller: _tabController,
+              children: [
+                Dashboard(),
+                Container(
+                  color: Colors.yellow,
+                ),
+                Container(
+                  color: Colors.green,
+                ),
+                Container(
+                  color: Colors.red,
+                ),
+                Container(
+                  color: Colors.cyanAccent,
+                ),
+                Container(
+                  color: Colors.orange,
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class DrawerItemButton extends StatelessWidget {
+  final Function() onTap;
+  final bool inverse;
+  final IconData iconData;
+  final String label;
+  const DrawerItemButton({
+    super.key,
+    required this.onTap,
+    this.inverse = true,
+    required this.iconData,
+    required this.label,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: inverse ? Color(0xFFE7EEE7) : Colors.black,
+      child: InkWell(
+        onTap: onTap,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+          child: Row(
+            children: [
+              Icon(
+                iconData,
+                color: inverse ? Color(0xFF7F00FF) : Colors.white,
+              ),
+              SizedBox(
+                width: 10,
+              ),
+              Text(
+                label,
+                style: TextStyle(
+                    color: inverse ? Color(0xFF7F00FF) : Colors.white),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
